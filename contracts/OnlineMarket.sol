035e4cf1f7f50960ade4fa2fd8180439315f2281
Romil Jain <romil2707@gmail.com>
2019-07-02 19:26:40 -0700

    Added code for Admin functionalities

--- contracts/OnlineMarket.sol
+++ contracts/OnlineMarket.sol
@@ -1,6 +1,9 @@
 pragma solidity >=0.4.0 <0.7.0;
 
 contract OnlineMarket{
+    constructor() public{
+        admins[msg.sender] = true;
+    }
     
     //Owner
     address owner;
@@ -16,73 +19,31 @@ contract OnlineMarket{
     
     // Hold the Store Owners who are approved
     address[] private approvedStoreOwners;
+
+    modifier onlyOwner(){
+        require(msg.sender == owner);
+        _;
+    }
     
-    constructor() public{
-        admins[msg.sender] = true;
+    modifier onlyAdmin(){
+        require(admins[msg.sender] == true);
+        _;
     }
     
     // Add admin
-    function addAdmin(address adminAddress) public {
-
+    function addAdmin(address adminAddress) public onlyAdmin{
+        admins[adminAddress] = true;
     }
     
     // Remove Admin
-    function removeAdmin(address adminAddress) public {
-    
+    function removeAdmin(address adminAddress) public onlyOwner{
+        require(admins[adminAddress] == true);
+        admins[adminAddress] = false;
     }
     
     //Check if Admin
     function checkAdmin(address adminAddress) public view returns(bool){
-        
-    }
-    
-    // Below function is to view a requested StoreOwners at index by Admin
-    function viewRequestedStoreOwner(uint index) public view returns (address){
-        
-    }
-    
-    // Below function is to view all requested StoreOwners by Admin
-    function viewRequestedStoreOwners() public view returns (address[] memory){
-        
-    }
-    
-    // Below function is to view a approved StoreOwners at index by Admin
-    function viewApprovedStoreOwner(uint index) public view returns (address){
-        
-    }
-    
-    // Below function is to view all approved StoreOwners by Admin
-    function viewApprovedStoreOwners() public view returns (address[] memory){
-        
-    }
-    
-    // Below function is to approve the Stores by Admin
-    function approveStoreOwners(address storeOwner) public {
-        
-    }
-    
-    // Below function is to remove the storeOwnerAddress by Admin
-    function removeStoreOwner(address storeOwner) public {
-        
-    }
-    
-    function checkStoreOwnerStatus(address storeOwner) public view returns(bool){
-        
-    }
-    
-    // Add storeOwner
-    function addStoreOwner() public {
-        
-    }
-    
-    // get requested store owners length
-    function getRequestedStoreOwnersLength() public view returns(uint){
-        
-    }
-    
-    // get approved store owners length
-    function getApprovedStoreOwnersLength() public view returns(uint){
-        
+        return admins[adminAddress];
     }
     
 }

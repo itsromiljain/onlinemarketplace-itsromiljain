0943a882475b5287de244b1e383e0493f67bf2fd
Romil Jain <romil2707@gmail.com>
2019-07-05 16:53:50 -0700

    Added StoreFront contract in deploy contract js to deploy OnlineMarket and StoreFront contracts

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,8 @@
 var OnlineMarket = artifacts.require("OnlineMarket");
+var StoreFront = artifacts.require("StoreFront.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(OnlineMarket);
+  deployer.deploy(OnlineMarket).then(function() {
+  	return deployer.deploy(StoreFront, OnlineMarket.address);
+  });
 };

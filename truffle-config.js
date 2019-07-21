23206a1dd5c87784bad021d33bdfd3324bdbf2f5
Romil Jain <romil2707@gmail.com>
2019-07-20 19:51:25 -0700

    Added Rinkeby Infura configurations

--- truffle-config.js
+++ truffle-config.js
@@ -1,3 +1,10 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const infuraKey = "85ba70deebdd408b95718a4e5b7166cb";
+
+const fs = require('fs');
+const mnemonic = fs.readFileSync(".secret").toString().trim();
+const infuraURL = 'https://rinkeby.infura.io/v3/85ba70deebdd408b95718a4e5b7166cb';
+
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // for more about customizing your Truffle configuration!
@@ -9,7 +16,12 @@ module.exports = {
     },
     develop: {
       port: 8545
-    }
+    },
+     rinkeby: {
+      provider: () => new HDWalletProvider(mnemonic, infuraURL),     // Localhost (default: none)
+      network_id: 4,            // Rinkeby's network id
+      gas: 6500000,       // Any network (default: none)
+     },
   },
   compilers: {
     solc: {

0b1ec512dfc39a73431129118464c00da7936d69
Romil Jain <romil2707@gmail.com>
2019-08-10 23:59:37 -0700

    Commenting the HDWalletProvider in truffle-config.js as it may cause some difficulty to run the project.

--- truffle-config.js
+++ truffle-config.js
@@ -1,9 +1,9 @@
-const HDWalletProvider = require('truffle-hdwallet-provider');
-const infuraKey = "85ba70deebdd408b95718a4e5b7166cb";
+//const HDWalletProvider = require('truffle-hdwallet-provider');
+//const infuraKey = "85ba70deebdd408b95718a4e5b7166cb";
 
-const fs = require('fs');
-const mnemonic = fs.readFileSync(".secret").toString().trim();
-const infuraURL = 'https://rinkeby.infura.io/v3/85ba70deebdd408b95718a4e5b7166cb';
+//const fs = require('fs');
+//const mnemonic = fs.readFileSync(".secret").toString().trim();
+//const infuraURL = 'https://rinkeby.infura.io/v3/85ba70deebdd408b95718a4e5b7166cb';
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
@@ -17,11 +17,11 @@ module.exports = {
     develop: {
       port: 8545
     },
-     rinkeby: {
+     /*rinkeby: {
       provider: () => new HDWalletProvider(mnemonic, infuraURL),     // Localhost (default: none)
       network_id: 4,            // Rinkeby's network id
       gas: 6500000,       // Any network (default: none)
-     },
+     },*/
   },
   compilers: {
     solc: {
